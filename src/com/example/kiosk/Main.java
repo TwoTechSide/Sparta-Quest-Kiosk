189aaf3e16a4514d3b3f4f673994593dca851c0b
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-07-18 15:30:00 +0900

    refactor: rename burger list

--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -12,9 +12,9 @@ public class Main {
 
         while (true) {
             menuItems.add(new MenuItem("ShackBurger", 6.9f, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-            menuItems.add(new MenuItem("ShackBurger", 8.9f, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-            menuItems.add(new MenuItem("ShackBurger", 6.9f, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-            menuItems.add(new MenuItem("ShackBurger", 5.4f, "비프패티를 기반으로 야채가 들어간 기본버거"));
+            menuItems.add(new MenuItem("SmokeShack", 8.9f, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+            menuItems.add(new MenuItem("Cheeseburger", 6.9f, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+            menuItems.add(new MenuItem("Hamburger", 5.4f, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
             System.out.println("[ SHAKESHACK MENU ]");
             int num = 1;
