a8850e4faba68d01293afa635041995cd50d9d4f
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-07-22 03:41:36 +0900

    #3 feat: Lv1 / 특정 입력을 받을 때까지 while문으로 반복 실행

--- src/com/example/kiosk/Lv1/Main.java
+++ src/com/example/kiosk/Lv1/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        boolean flag = true;
 
         MenuItem[] menu = new MenuItem[4];
         menu[0] = new MenuItem("ShackBurger", 6.9f, "토마토, 양상추, 쉑소스가 토핑된 치즈버거");
@@ -13,22 +14,24 @@ public class Main {
         menu[2] = new MenuItem("ShackBurger", 6.9f, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
         menu[3] = new MenuItem("ShackBurger", 5.4f, "비프패티를 기반으로 야채가 들어간 기본버거");
 
-        System.out.println("[ SHAKESHACK MENU ]");
-        for (int i = 0; i < menu.length; i++) {
-            System.out.print((i+1) + ". " + menu[i].getName() + "\t");
-            System.out.print("| W " + menu[i].getPrice() + "\t");
-            System.out.print("| " + menu[i].getDescription() + "\n");
-        }
-        System.out.println("0. 종료");
+        while (flag) {
+            System.out.println("[ SHAKESHACK MENU ]");
+            for (int i = 0; i < menu.length; i++) {
+                System.out.print((i+1) + ". " + menu[i].getName() + "\t");
+                System.out.print("| W " + menu[i].getPrice() + "\t");
+                System.out.print("| " + menu[i].getDescription() + "\n");
+            }
+            System.out.println("0. 종료");
 
-        System.out.print("입력 : ");
-        int selectNum = sc.nextInt();
+            System.out.print("입력 : ");
+            int selectNum = sc.nextInt();
 
-        System.out.println();
+            System.out.println();
 
-        if (selectNum == 0) {
-            System.out.println("프로그램을 종료합니다.");
-            return;
+            if (selectNum == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                flag = false;
+            }
         }
     }
 }
