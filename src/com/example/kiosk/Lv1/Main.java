3cb6f850e7cccf54af108a90c6f7cea333be2513
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-07-24 08:27:22 +0900

    #15 fix: Lv1의 menu 배열에 접근할 수 있도록 for문 범위 수정

--- src/com/example/kiosk/Lv1/Main.java
+++ src/com/example/kiosk/Lv1/Main.java
@@ -19,8 +19,8 @@ public class Main {
         while (flag) {
             // 메뉴 목록 출력
             System.out.println("[ SHAKESHACK MENU ]");
-            for (int i = 1; i <= menu.length; i++) {
-                System.out.print(i + ". " + menu[i].getName() + "\t");
+            for (int i = 0; i < menu.length; i++) {
+                System.out.print((i+1) + ". " + menu[i].getName() + "\t");
                 System.out.print("| W " + menu[i].getPrice() + "\t");
                 System.out.print("| " + menu[i].getDescription() + "\n");
             }
