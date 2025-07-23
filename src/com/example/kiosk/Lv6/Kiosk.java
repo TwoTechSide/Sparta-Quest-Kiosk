95e922ce4d5217a4f7c7deaf2f76b95c63e94785
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-07-23 10:38:27 +0900

    #16 feat: Kiosk의 flag를 통해 조건 분기

--- src/com/example/kiosk/Lv6/Kiosk.java
+++ src/com/example/kiosk/Lv6/Kiosk.java
@@ -9,7 +9,7 @@ public class Kiosk {
 
     private final List<Menu> menus;
     private final List<MenuItem> selectedMenuItems = new ArrayList<>();
-    private boolean flag = true;
+    private int flag = 0;
 
     // 키오스크 생성자 -> 각 카테고리별 메뉴가 담긴 리스트 불러오기
     public Kiosk(List<Menu> menus) {
@@ -44,53 +44,76 @@ public class Kiosk {
     public void start() {
         Scanner sc = new Scanner(System.in);
         int selectNum;
+        Menu selectedMenu = null;
+        MenuItem selectedMenuItem = null;
 
-        while (flag) {
+        while (flag > -1) {
             try {
-                // 카테고리 선택
-                printCategories();
-                selectNum = sc.nextInt();
-                Menu selectedMenu;
+                // flag == 0 -> 카테고리 선택
+                if (flag == 0) {
+                    // 카테고리 선택 화면 출력
+                    printCategories();
 
-                // 0 : 키오스크 종료
-                // 1 ~ n : menus : List<Menu>에서 선택
-                // 나머지 : InputMismatchException 으로 예외 처리
-                if (selectNum == 0) {
-                    flag = false;
-                    break;
-                } else if (selectNum < menus.size()) {
-                    selectedMenu = menus.get(selectNum-1);
-                } else {
-                    throw new InputMismatchException();
+                    // 입력값에 따라 프로그램을 종료하거나 특정 카테고리의 메뉴 선택으로 이동
+                    selectNum = sc.nextInt();
+
+                    if (selectNum == 0) {
+                        flag = -1;
+                        break;
+                    }
+                    else if (selectNum <= menus.size()) {
+                        selectedMenu = menus.get(selectNum);
+                        flag = 1;
+                    }
+                    else throw new InputMismatchException();
                 }
 
-                // 메뉴 선택
-                printMenuItems(selectedMenu);
-                selectNum = sc.nextInt();
+                // flag == 1 -> 메뉴 선택
+                else if (flag == 1) {
+                    // 메뉴 선택 화면 출력
+                    // 만약 메뉴 선택이 이루어지지 않는 버그가 발생하는 경우 처음부터 다시 진행
+                    if (selectedMenu != null) printMenuItems(selectedMenu);
+                    else { flag = 0; continue; }
 
-                // 0 : 카테고리 선택 화면으로 돌아가기
-                // 1 ~ n : selectedMenu : List<Menu>에서 선택
-                // 나머지 : InputMismatchException 으로 예외 처리
-                if (selectNum == 0) {
-                    continue;
-                } else if (selectNum < selectedMenu.getMenuItems().size()) {
-                    MenuItem selectedMenuItem = selectedMenu.getMenuItem(selectNum-1);
-                    selectedMenuItems.add(selectedMenuItem);
-                } else {
-                    throw new InputMismatchException();
+                    // 0 : 카테고리 선택으로 이동
+                    // 메뉴 : 장바구니 추가 확인으로 이동
+                    // 그 외 : 잘못된 입력 예외 처리 -> 재확인
+                    selectNum = sc.nextInt();
+
+                    if (selectNum == 0) flag = 1;
+                    else if (selectNum <= selectedMenu.getMenuItems().size()) { selectedMenuItem = selectedMenu.getMenuItem(selectNum-1); flag = 2; }
+                    else throw new InputMismatchException();
                 }
 
-                // 선택한 메뉴들 출력
-                System.out.println("------- 선택한 메뉴 -------");
-                for (MenuItem menuItem : selectedMenuItems) { System.out.println(menuItem.toString()); }
-                System.out.println("-------------------------");
+                // flag == 2 -> 장바구니 추가 확인
+                else if (flag == 2) {
+                    if (selectedMenuItem != null) {
+                        String selectedMenuItemInfo = String.format("%s | W %.1f | %s", selectedMenuItem.getName(), selectedMenuItem.getPrice(), selectedMenuItem.getDescription());
+                        System.out.println("선택한 메뉴: " + selectedMenuItemInfo);
+                    } else { flag = 0; continue; }
+
+                    System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+                    System.out.println("1. 확인\t\t2.취소");
+
+                    // 1 : 장바구니에 추가
+                    // 2 : 장바구니 취소, 메뉴판으로 이동
+                    // 그 외 : 잘못된 입력이므로 다시 확인
+                    selectNum = sc.nextInt();
+
+                    if (selectNum == 1) { selectedMenuItems.add(selectedMenuItem); flag = 0; }
+                    else if (selectNum == 2) flag = 1;
+                    else throw new InputMismatchException();
+                }
             }
-            // 메뉴에 있는 숫자를 벗어난 값을 받는 경우(=IndexOutOfBoundsException), 정수가 아닌 값을 받는 경우(=InputMismatchException) 예외 처리
+            // 메뉴에 있는 숫자를 벗어난 값을 받는 경우(=IndexOutOfBoundsException), 정수가 아닌 값을 받는 경우(=InputMismatchException)
+            // 예외 처리하고, 예외가 발생한 flag로 재이동
             catch (IndexOutOfBoundsException e) {
                 System.out.println("[오류] : 메뉴를 조회하는데 실패하였습니다. 다시 시도해주세요.");
             } catch (InputMismatchException e) {
                 System.out.println("[오류] : 잘못된 입력입니다.");
-                sc.next();
+                sc.nextLine();
+            } finally {
+                System.out.println();
             }
         }
     }
